899df8f333d9bf8b258057500d9d28b27d5b07a4
Andrew Fray <andrew@spryfox.com>
2013-05-13 11:23:44 +0100

    c-sharp mode now imported through elpa

--- .emacs.d/required-packages.el
+++ .emacs.d/required-packages.el
@@ -1,6 +1,6 @@
 (defvar required-packages
   '(ace-jump-mode expand-region magit no-easy-keys projectile 
-		  color-theme-solarized json-mode flycheck ag icicles)
+		  color-theme-solarized json-mode flycheck ag icicles csharp-mode)
   "A list of packages to ensure are installed at launch.")
 
 (defun required-packages-installed-p ()
