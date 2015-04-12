070f9a6b1455a827b2e0af0d018090325b002049
Andrew Fray <andrew.fray@gmail.com>
2015-04-12 21:57:36 +0100

    select prelude modules
    including helm everywhere

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -2,9 +2,9 @@
 
 ;; Emacs IRC client
 (require 'prelude-erc)
-(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
-;; (require 'prelude-helm) ;; Interface for narrowing and search
-;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+;;(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-mediawiki)
@@ -26,13 +26,13 @@
 (require 'prelude-lisp)
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
-(require 'prelude-perl)
-;; (require 'prelude-python)
+;; (require 'prelude-perl)
+(require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
-(require 'prelude-scheme)
+;; (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
-;; (require 'prelude-yaml)
+(require 'prelude-yaml)
